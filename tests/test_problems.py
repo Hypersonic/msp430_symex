4be5884d828cf3369106b017669fe0b75f44f7df
Josh Hofing <joshhofing@gmail.com>
2017-03-21 01:16:06 -0400

    Add Sydney problem testcase

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -61,7 +61,7 @@ class TestTutorial(unittest.TestCase):
 class TestNewOrleans(unittest.TestCase):
     def test_new_orleans():
         """
-        Test solving the tutorial level of microcorruption.
+        Test solving the New Orleans level of microcorruption.
         """
         dump = \
             "0000:   0000 4400 0000 0000 0000 0000 0000 0000   ..D.............\n" \
@@ -115,6 +115,63 @@ class TestNewOrleans(unittest.TestCase):
         winning_input = unlocked_state.sym_input.dump(unlocked_state).rstrip(b'\xc0')
 
         self.assertEqual(winning_input, b'3E0#*nv\x00')
+
+
+class TestSydney(unittest.TestCase):
+    def test_sydney():
+        """
+        Test solving the Sydney level of microcorruption.
+        """
+        dump = \
+            "0000:   0000 4400 0000 0000 0000 0000 0000 0000   ..D.............\n" \
+            "0010:   *\n" \
+            "4400:   3140 0044 1542 5c01 75f3 35d0 085a 3f40   1@.D.B\.u.5..Z?@\n" \
+            "4410:   0000 0f93 0724 8245 5c01 2f83 9f4f 9445   .....$.E\./..O.E\n" \
+            "4420:   0024 f923 3f40 0000 0f93 0624 8245 5c01   .$.#?@.....$.E\.\n" \
+            "4430:   1f83 cf43 0024 fa23 3150 9cff 3f40 b444   ...C.$.#1P..?@.D\n" \
+            "4440:   b012 6645 0f41 b012 8044 0f41 b012 8a44   ..fE.A...D.A...D\n" \
+            "4450:   0f93 0520 3f40 d444 b012 6645 093c 3f40   ... ?@.D..fE.<?@\n" \
+            "4460:   f144 b012 6645 3012 7f00 b012 0245 2153   .D..fE0....E!S\n" \
+            "4470:   0f43 3150 6400 32d0 f000 fd3f 3040 9245   .C1Pd.2....?0@.E\n" \
+            "4480:   3e40 6400 b012 5645 3041 bf90 2555 0000   >@d...VE0A..%U..\n" \
+            "4490:   0d20 bf90 402b 0200 0920 bf90 4450 0400   . ..@+... ..DP..\n" \
+            "44a0:   0520 1e43 bf90 6f27 0600 0124 0e43 0f4e   . .C..o'...$.C.N\n" \
+            "44b0:   3041 3041 456e 7465 7220 7468 6520 7061   0A0AEnter the pa\n" \
+            "44c0:   7373 776f 7264 2074 6f20 636f 6e74 696e   ssword to contin\n" \
+            "44d0:   7565 2e00 496e 7661 6c69 6420 7061 7373   ue..Invalid pass\n" \
+            "44e0:   776f 7264 3b20 7472 7920 6167 6169 6e2e   word; try again.\n" \
+            "44f0:   0041 6363 6573 7320 4772 616e 7465 6421   .Access Granted!\n" \
+            "4500:   0000 1e41 0200 0212 0f4e 8f10 024f 32d0   ...A.....N...O2.\n" \
+            "4510:   0080 b012 1000 3241 3041 2183 0f12 0312   ......2A0A!.....\n" \
+            "4520:   814f 0400 b012 0245 1f41 0400 3150 0600   .O.....E.A..1P..\n" \
+            "4530:   3041 0412 0441 2453 2183 3f40 fcff 0f54   0A...A$S!.?@...T\n" \
+            "4540:   0f12 1312 b012 0245 5f44 fcff 8f11 3150   .......E_D....1P\n" \
+            "4550:   0600 3441 3041 0e12 0f12 2312 b012 0245   ..4A0A....#....E\n" \
+            "4560:   3150 0600 3041 0b12 0b4f 073c 1b53 8f11   1P..0A...O.<.S..\n" \
+            "4570:   0f12 0312 b012 0245 2152 6f4b 4f93 f623   .......E!RoKO..#\n" \
+            "4580:   3012 0a00 0312 b012 0245 2152 0f43 3b41   0........E!R.C;A\n" \
+            "4590:   3041 0013 0000 0000 0000 0000 0000 0000   0A..............\n" \
+            "45a0:   *\n" \
+            "ff80:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 7c44   |D|D|D|D|D|D|D|D\n" \
+            "ff90:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 7c44   |D|D|D|D|D|D|D|D\n" \
+            "ffa0:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 7c44   |D|D|D|D|D|D|D|D\n" \
+            "ffb0:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 7c44   |D|D|D|D|D|D|D|D\n" \
+            "ffc0:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 7c44   |D|D|D|D|D|D|D|D\n" \
+            "ffd0:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 7c44   |D|D|D|D|D|D|D|D\n" \
+            "ffe0:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 7c44   |D|D|D|D|D|D|D|D\n" \
+            "fff0:   7c44 7c44 7c44 7c44 7c44 7c44 7c44 0044   |D|D|D|D|D|D|D.D"
+
+
+        #TODO: automatically find avoid address via CFG
+        pg = start_path_group(dump, 0x4400, avoid=0x4454)
+
+        pg.step_until_unlocked()
+
+        unlocked_state = list(pg.unlocked)[0]
+
+        winning_input = unlocked_state.sym_input.dump(unlocked_state).rstrip(b'\xc0')
+
+        self.assertEqual(winning_input, b"%U@+DPo'")
 
 
 if __name__ == '__main__':
