0468a47d0495ab5265ad8d53a77c10687377ad15
Josh Hofing <joshhofing@gmail.com>
2017-03-24 03:55:55 -0400

    Add JNZ semantics test

--- tests/test_instruction_semantics.py
+++ tests/test_instruction_semantics.py
@@ -5,6 +5,11 @@ from z3 import simplify
 from msp430_symex.state import State, blank_state
 from msp430_symex.code import decode_instruction
 
+def intval(v):
+    if isinstance(v, int):
+        return v
+    return simplify(v).as_long()
+
 # TODO: Test these!!
 """
 RRC
@@ -14,7 +19,36 @@ SXT
 PUSH
 CALL
 RETI
-JNZ
+"""
+
+class TestJnzInstruction(unittest.TestCase):
+
+    def test_instruction_jnz(self):
+        # jnz #0x1446
+        raw = b'\x08\x21'
+        ip = 0x1234
+
+        ins, ins_len = decode_instruction(ip, raw)
+
+        state = blank_state()
+        state.cpu.registers['R0'] = ip + ins_len # ip is always preincremented
+
+        expected_taken = 0x1446
+        expected_not_taken = 0x1236
+
+        new_states = state.cpu.step_jnz(state, ins)
+
+        self.assertEqual(len(new_states), 2)
+
+        taken_states = [st for st in new_states if intval(st.cpu.registers['R0']) == expected_taken]
+        not_taken_states = [st for st in new_states if intval(st.cpu.registers['R0']) == expected_not_taken]
+
+        self.assertEqual(len(taken_states), 1)
+        self.assertEqual(len(not_taken_states), 1)
+
+
+# TODO: Test these!!
+"""
 JZ
 JNC
 JC
