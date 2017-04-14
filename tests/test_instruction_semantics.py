ca9178a9689333f3d5ad0a6ac1a5dea4c4bef8c3
Josh Hofing <joshhofing@gmail.com>
2017-04-14 18:13:19 -0400

    Add zflag tests for ADD instruction

--- tests/test_instruction_semantics.py
+++ tests/test_instruction_semantics.py
@@ -362,7 +362,42 @@ class TestAddInstruction(unittest.TestCase):
             n_flag = (flag_reg & st.cpu.registers.mask_N) != 0
             self.assertFalse(n_flag)
 
-    # TODO: Z flag
+    def test_instruction_semantics_add_zflag_set(self):
+        # add #0x0123, r15
+        raw = b'\x3f\x50\x23\x01'
+        ip = 0x1234
+
+        ins, _ = decode_instruction(ip, raw)
+
+        state = blank_state()
+        state.cpu.registers['R15'] = BitVecVal(0xfedd, 16)
+
+        new_states = state.cpu.step_add(state, ins, enable_unsound_optimizations=False)
+        new_states = [st for st in new_states if st.path.is_sat()] # only sat states
+
+        for st in new_states:
+            flag_reg = intval(st.cpu.registers['R2'])
+            z_flag = (flag_reg & st.cpu.registers.mask_Z) != 0
+            self.assertTrue(z_flag)
+
+    def test_instruction_semantics_add_zflag_unset(self):
+        # add #0x0123, r15
+        raw = b'\x3f\x50\x23\x01'
+        ip = 0x1234
+
+        ins, _ = decode_instruction(ip, raw)
+
+        state = blank_state()
+        state.cpu.registers['R15'] = BitVecVal(0x0123, 16)
+
+        new_states = state.cpu.step_add(state, ins, enable_unsound_optimizations=False)
+        new_states = [st for st in new_states if st.path.is_sat()] # only sat states
+
+        for st in new_states:
+            flag_reg = intval(st.cpu.registers['R2'])
+            z_flag = (flag_reg & st.cpu.registers.mask_Z) != 0
+            self.assertFalse(z_flag)
+
     # TODO: C flag
     # TODO: V flag
 
